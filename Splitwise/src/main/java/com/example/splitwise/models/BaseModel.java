6d254f012a9b892f0d2b27e1003b7c3efafb0645
Ashish <asku3000@gmail.com>
2022-11-19 18:24:13 +0530

    Splitwise models

---
+++ Splitwise/src/main/java/com/example/splitwise/models/BaseModel.java
@@ -0,0 +1,26 @@
+package com.example.splitwise.models;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@Getter
+@MappedSuperclass
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date lastUpdatedAt;
+}
---
+++ Splitwise/src/main/java/com/example/splitwise/models/User.java
@@ -0,0 +1,15 @@
+package com.example.splitwise.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseModel{
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String hashedPasswords;
+}
